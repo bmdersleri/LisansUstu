e4f191ede3f2ecb5a65c87e23cbfe63e39a6e6c0
Bilgisayar Mühendisliği Derslerim <74094387+bmdersleri@users.noreply.github.com>
2023-03-12 14:31:01 +0300

    Add files via upload

---
+++ adım sayısı tespiti.mm
@@ -0,0 +1,262 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1678618870542" ID="ID_1832234666" MODIFIED="1678620182305" TEXT="B&#xfc;y&#xfc;kba&#x15f; hayvanlar i&#xe7;in ivme&#xf6;l&#xe7;er verilerinden sinyal i&#x15f;leme metodlar&#x131;yla ad&#x131;m say&#x131;s&#x131; tespiti">
+<icon BUILTIN="gohome"/>
+<node CREATED="1678618908492" ID="ID_986827915" MODIFIED="1678620560115" POSITION="right" TEXT="Giri&#x15f;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Giri&#351; b&#246;l&#252;m&#252;nde &#231;al&#305;&#351;man&#305;n motivasyon kayna&#287;&#305; &#246;rnekler &#252;zerinden a&#231;&#305;klanacak.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-1"/>
+<node CREATED="1678619137740" ID="ID_1886186669" MODIFIED="1678620111098" TEXT="&#x130;neklerde ad&#x131;m say&#x131;s&#x131;n&#x131;n bilinmesi neden &#xf6;nemlidir?">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#304;neklerde ad&#305;m saymak ni&#231;in &#231;ok &#246;nemlidir?
+    </p>
+    <p>
+      Ad&#305;m say&#305;s&#305;n&#305;n artmas&#305; veya azalmas&#305; neyi g&#246;sterir?
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1678619124755" ID="ID_1541850688" MODIFIED="1678620126394" TEXT="Ad&#x131;m&#xf6;l&#xe7;er nedir ve ne ama&#xe7;la kullan&#x131;l&#x131;r">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ad&#305;m &#246;l&#231;er nedir ne ama&#231;la kullan&#305;l&#305;r?
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1678619995850" ID="ID_705201410" MODIFIED="1678620150950" TEXT="Ad&#x131;m k&#x131;zg&#x131;nl&#x131;k ili&#x15f;kisi nedir?">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ad&#305;m say&#305;s&#305; ile k&#305;zg&#305;nl&#305;k aras&#305;nda nas&#305;l bir ili&#351;ki vard&#305;r?
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1678620015638" ID="ID_383167804" MODIFIED="1678620291069" TEXT="Ad&#x131;m say&#x131;s&#x131; hayvan refah&#x131; ili&#x15f;kisi nedir?">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Hayvan refah&#305; ile ad&#305;m say&#305;s&#305; aras&#305;ndaki ili&#351;kli irdelenecek.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1678620243429" ID="ID_1387383960" MODIFIED="1678620334318" TEXT="Literat&#xfc;rde yap&#x131;lm&#x131;&#x15f; benzer &#xe7;al&#x131;&#x15f;malar nelerdir?">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      G&#252;ncel literat&#252;rde (&#246;zellikle son 5 y&#305;l) yer alan benzer &#231;al&#305;&#351;malar hakk&#305;nda bilgi verilecek.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+<node CREATED="1678618944259" ID="ID_1765021810" MODIFIED="1678620558919" POSITION="right" TEXT="Metodoloji">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Metodoloji b&#246;l&#252;m&#252;nde &#231;al&#305;&#351;ma kapsam&#305;nda geli&#351;tirlen veya kullan&#305;lan y&#246;ntem hakk&#305;nda derinlemesine bilgi verilecek.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-2"/>
+<node CREATED="1678618917481" HGAP="25" ID="ID_890125420" MODIFIED="1678619632083" TEXT="Ad&#x131;m &#xf6;l&#xe7;erin &#xe7;al&#x131;&#x15f;ma prensibi" VSHIFT="16">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ad&#305;m &#246;l&#231;er nas&#305;l &#231;al&#305;&#351;&#305;r &#231;al&#305;&#351;ma prensipleri ve &#246;rnekleri burada anlat&#305;lacak.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1678619284285" ID="ID_1016895316" MODIFIED="1678619746473" TEXT="Genel sinyal i&#x15f;leme &#x15f;emas&#x131;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sinyal i&#351;lemenin ad&#305;mlar&#305; burada bir &#351;ekil olarak verilecek
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      1 Medyan filter
+    </p>
+    <p>
+      2 Normalizasyon
+    </p>
+    <p>
+      3 Savitzky-Golay
+    </p>
+    <p>
+      4 Tepelerin bulunmas&#305;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1678619341346" ID="ID_1457515280" MODIFIED="1678619789137" TEXT="Ham ad&#x131;m sinyallerinin g&#xf6;r&#xfc;nt&#xfc;s&#xfc; ">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Burada ivme&#246;l&#231;erden toplanan ham veriler g&#246;sterilecek.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1678619368779" ID="ID_678102970" MODIFIED="1678619819776" TEXT="Medyan filtresi ve kullan&#x131;m&#x131;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Medyan filtresinin form&#252;l&#252; verilecek ve sinyalin medyan filtresi sonras&#305; hali g&#246;sterilecek
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1678619396172" ID="ID_584769545" MODIFIED="1678619860711" TEXT="Sinyalin normalize edilmesi">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Normalizasyon i&#351;lemi tarfi edilecek ve normalizasyon sonras&#305; sinyal &#351;ekli verilecek
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1678619417182" ID="ID_344396019" MODIFIED="1678619883861" TEXT="Sinyalin yumu&#x15f;at&#x131;lmas&#x131;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Savitzky Golay filtresinin form&#252;l&#252; ve filtre &#231;&#305;k&#305;&#351;&#305; g&#246;sterilecek
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-6"/>
+</node>
+<node CREATED="1678619437970" ID="ID_1765822756" MODIFIED="1678619909755" TEXT="Ad&#x131;m say&#x131;s&#x131;n&#x131;n tayini">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tepelerin say&#305;lmas&#305; i&#351;lemi tarif edilecek ve ad&#305;m say&#305;s&#305;n&#305; g&#246;sterir sinyal &#351;ekli verilecek.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-7"/>
+</node>
+</node>
+<node CREATED="1678618976786" ID="ID_748740852" MODIFIED="1678619972314" POSITION="right" TEXT="Sonu&#xe7;lar ve De&#x11f;erlendirme">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Elde edilen &#246;l&#231;&#252;mler sonucunda tespit edilen ad&#305;m say&#305;lar&#305; ile kamera kay&#305;tlar&#305;ndan elde edilen ad&#305;m say&#305;lar&#305; kar&#351;&#305;la&#351;t&#305;rlacak ve hata oranlar&#305; hesaplanarak kar&#351;&#305;la&#351;t&#305;r&#305;lacak.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1678618987248" ID="ID_1029931914" MODIFIED="1678619540882" POSITION="right" TEXT="Te&#x15f;ekk&#xfc;r">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p class="MsoNormal" style="text-align: justify">
+      <font size="12.0pt" face="Times New Roman,serif">Bu &#231;al&#305;&#351;ma Burdur Mehmet Akif Ersoy &#220;niversitesi Bilimsel Ara&#351;t&#305;rma Projeleri Komisyonunca desteklenmi&#351;tir. Proje No: 0739-YL-21<o p="#DEFAULT" style="line-height: 115%"></o></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1678619195192" ID="ID_131433058" MODIFIED="1678620381815" POSITION="right" TEXT="Kaynak&#xe7;a">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Kaynak&#231;a sitiline mutlaka dikkat edilemeli, b&#252;t&#252;n kaynaklar zotero kullan&#305;larak eklenmelidir.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+</map>
